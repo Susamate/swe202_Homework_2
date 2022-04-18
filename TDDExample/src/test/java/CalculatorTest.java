9af0a5da469b785201bd8e82cf0f34c47b62ced0
Susamate <74826099+Susamate@users.noreply.github.com>
2022-04-18 15:16:15 +0300

    Add files via upload

---
+++ TDDExample/src/test/java/CalculatorTest.java
@@ -0,0 +1,35 @@
+/*
+B201202012
+HALİL İBRAHİM ÜSTÜN
+SWE202 SOFTWARE VERIFICATION AND VALIDATION
+HOMEWORK 2
+https://github.com/Susamate/swe202_Homework_2.git
+*/
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @ParameterizedTest
+    @CsvSource({"10,2,5","10,4,2.5","12.5,5,2.5","10,2.5,4","12.5,2.5,5"})
+    void collectionOfDivisions(double a, double b, double result){
+        assertEquals(result, Calculator.divide(a,b));
+
+    }
+
+}
